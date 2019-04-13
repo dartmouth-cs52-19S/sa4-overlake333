aa96605c17e9edd292876c6233f0aed1cde805d1
overlake333 <taylor.m.olson.21@dartmouth.edu>
2019-04-12 22:25:40 -0400

    fully deployed application

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+const startTime = new Date();
+
+const time = (initTime) => {
+  const currTime = new Date();
+  return Math.floor((currTime - initTime) / 1000);
+};
+
+setInterval(() => { $('#main').html(`You've been on this page for ${time(startTime)} seconds.`); }, 1000);
