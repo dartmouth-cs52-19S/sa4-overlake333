a900cf19cb2e88b73801305d0297ff9e813141e2
overlake333 <taylor.m.olson.21@dartmouth.edu>
2019-04-16 09:37:50 -0400

    final commit

---
+++ src/components/video_detail.js
@@ -0,0 +1,27 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from 'react';
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  } else {
+    const { videoId } = video.id;
+
+    const url = `https://www.youtube.com/embed/${videoId}`;
+
+    return (
+      <div id="video-detail">
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src={url} />
+        </div>
+
+        <div className="details">
+          <div>{video.snippet.title}</div>
+          <div>{video.snippet.description}</div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default VideoDetail;
